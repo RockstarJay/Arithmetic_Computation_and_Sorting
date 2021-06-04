dc95a47a9d67db53209366ef4409f3836c031fa2
unknown <RockstarJay>
2021-06-04 14:26:03 +0530

    The first computation a+b*c added

--- arithmeticComputationSorting.sh
+++ arithmeticComputationSorting.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 echo "Sorting Arithmetic Computation Problem"
+
 read -p "Enter The value for a : " a
 read -p "Enter The value for b : " b
 read -p "Enter The value for c : " c
-echo "a:$a  b:$b  c:$c"
+
+compute1=$((a+b*c))
+echo "The First Computation : "$compute1
