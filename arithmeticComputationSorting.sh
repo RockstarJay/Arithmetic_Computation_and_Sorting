ece2777e481e29e60657cbd1e2b23e48c8f63fda
unknown <RockstarJay>
2021-06-04 14:34:25 +0530

    The second computation a*b+c added

--- arithmeticComputationSorting.sh
+++ arithmeticComputationSorting.sh
@@ -6,4 +6,6 @@ read -p "Enter The value for b : " b
 read -p "Enter The value for c : " c
 
 compute1=$((a+b*c))
+compute2=$((a*b+c))
 echo "The First Computation : "$compute1
+echo "The Second Computation : "$compute2
