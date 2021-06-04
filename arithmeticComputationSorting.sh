744351b82e4acd41b3913eb44d238f2c471f726c
unknown <RockstarJay>
2021-06-04 14:48:06 +0530

    The fourth computation a%b+c added

--- arithmeticComputationSorting.sh
+++ arithmeticComputationSorting.sh
@@ -8,7 +8,8 @@ read -p "Enter The value for c : " c
 compute1=$((a+b*c))
 compute2=$((a*b+c))
 compute3=$((c+a/b))
+compute4=$((a%b+c))
 echo "The First Computation : "$compute1
 echo "The Second Computation : "$compute2
 echo "The Third Computation : "$compute3
-
+echo "The Fourth Computation : "$compute4
