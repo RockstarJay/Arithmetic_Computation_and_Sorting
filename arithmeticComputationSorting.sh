efcbd40189d30cff178f87132693e234accbc353
unknown <RockstarJay>
2021-06-04 14:18:29 +0530

    Taking the three inputs from user code added

--- arithmeticComputationSorting.sh
+++ arithmeticComputationSorting.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
 echo "Sorting Arithmetic Computation Problem"
+read -p "Enter The value for a : " a
+read -p "Enter The value for b : " b
+read -p "Enter The value for c : " c
+echo "a:$a  b:$b  c:$c"
